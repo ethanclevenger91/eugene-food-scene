ba7bede588ed53df2bc62092d7f7059e7a764a36
Mark Davis <311063+nohorse@users.noreply.github.com>
2020-05-12 23:48:16 -0700

    added contribution markdown

---
+++ EugeneFoodScene/Data/contribute.md
@@ -0,0 +1,13 @@
+﻿## Contribute
+
+This is your site!  You can make it better!  
+
+The easiest way is to provide fresh content!  If you have a business logo, a picture of a menu, or an updated special, use the form on the right.
+
+We are also looking for  contributors.  If you have project management skills, enjoy wranging data, like scouring Facebook groups, or know your way around CSS, we'd love to have you on the team.  
+
+You can find us in the [proj-eugene-food-scene channel](https://eugenetech.slack.com/archives/C0111JBQB63) on the [EugeneTech Slack](http://eugslack.com) 
+
+Check out our [Roadmap](https://www.notion.so/380b25da96f34287a73e7fc942f1c138?v=1d2adefd11744928bc711d3962c857e1) or see the [about page](/about) for more details.
+
+If you would like to become a backer or sponsor, check out our [Sustainability Page](https://opencollective.com/open-eugene)
---
+++ EugeneFoodScene/Shared/MarkdownComponent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ganss.XSS;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace EugeneFoodScene.Data
+{
+    public class MarkdownComponent : ComponentBase
+    {
+        private string _content;
+
+       // [Inject] public IHtmlSanitizer HtmlSanitizer { get; set; }
+
+        [Parameter]
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                HtmlContent = ConvertStringToMarkupString(_content);
+            }
+        }
+
+        public MarkupString HtmlContent { get; private set; }
+
+        private MarkupString ConvertStringToMarkupString(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(_content))
+            {
+                // Convert markdown string to HTML
+                var html = Markdig.Markdown.ToHtml(value,
+                    new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+
+                var htmlSanitizer = new HtmlSanitizer();
+                // Sanitize HTML before rendering
+                var sanitizedHtml = htmlSanitizer.Sanitize(html);
+
+                // Return sanitized HTML as a MarkupString that Blazor can render
+                return new MarkupString(sanitizedHtml);
+            }
+
+            return new MarkupString();
+        }
+    }
+}
